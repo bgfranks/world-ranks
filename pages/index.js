5c67e96d6ac7e739498f3ad9f6278d220b8a1deb
Brandon Franks <Brandon@Brandons-MacBook-Pro.local>
2020-11-28 18:58:53 -0500

    fetching the country data with static props and imported material ui core and icons

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
 import Layout from "../components/Layout/Layout"
+import styled from "styled-components"
 
-export default function Home() {
-  return <Layout>main</Layout>
+export default function Home({ countries }) {
+  console.log(countries)
+  return (
+    <Layout>
+      <div>Found {countries.length} countries</div>
+    </Layout>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all")
+  const countries = await res.json()
+
+  return {
+    props: {
+      countries,
+    },
+  }
 }
