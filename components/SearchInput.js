495e7ed4ac374d00d2d7ccec5f9c689fbc1d5838
Brandon Franks <Brandon@Brandons-MacBook-Pro.local>
2020-11-28 19:28:29 -0500

    converted the global variables to styled components and started styling the search bar

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,7 +1,35 @@
-const SearchInput = () => {
+import SearchRounded from "@material-ui/icons/SearchRounded"
+import styled from "styled-components"
+
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: var(--background-color-dark);
+  border-radius: 8px;
+  padding-left: 16px;
+  color: var(--text-color-secondary);
+
+  .input {
+    border: none;
+    background-color: transparent;
+    padding: 16px;
+    width: 100%;
+    height: 100%;
+    outline: none;
+
+    ::placeholder {
+      color: var(--text-color-secondary);
+    }
+  }
+`
+
+const SearchInput = ({ ...rest }) => {
   return (
-    <div>
-      <input />
-    </div>
+    <SearchContainer>
+      <SearchRounded />
+      <input className="input" {...rest} />
+    </SearchContainer>
   )
 }
+
+export default SearchInput
