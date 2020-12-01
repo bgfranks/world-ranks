14a07675382b46fa4e8ce240984f9a024a79c480
Brandon Franks <Brandon@Brandons-MacBook-Pro.local>
2020-11-30 22:41:36 -0500

    created a way to filter by country, region, and subregion. also fixed a bug with the sorting of name and population

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import Layout from "../components/Layout/Layout"
 import SearchInput from "../components/SearchInput"
 
@@ -10,11 +12,29 @@ const Count = styled.div`
 `
 
 export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("")
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  )
+
+  const onInputChange = (e) => {
+    e.preventDefault()
+
+    setKeyword(e.target.value.toLowerCase())
+  }
+
   return (
     <Layout>
       <Count>Found {countries.length} countries</Count>
-      <SearchInput placeholder="Search by Name, Region, or SubRegion" />
-      <CountriesTable countries={countries} />
+      <SearchInput
+        placeholder="Search by Name, Region, or SubRegion"
+        onChange={onInputChange}
+      />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   )
 }
